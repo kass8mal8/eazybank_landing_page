2f9d8f449d973ac17494ae876f81e4ff428036f0
kass8mal8 <kassimaly21@gmail.com>
2022-09-20 19:04:56 +0300

    made final touches to program

--- code/script.js
+++ code/script.js
@@ -1,3 +1,19 @@
+
+
 const toggleMenu = () =>{
     document.querySelector('.sidebar').classList.toggle('active')
+    document.querySelector('.menu').style.display = "none"
+    let close = document.querySelector(".close")
+
+    close.style.display = "block"  
+
+    close.addEventListener('click',() => {
+        document.querySelector('.menu').style.display = "block"
+        document.querySelector('.sidebar').classList.toggle('active')
+        close.style.display = "none"  
+    })
+    
 }
+
+
+
